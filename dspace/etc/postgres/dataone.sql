965440718eab650061b2edfb2314e69a6a80de0c
Mark Diggory <mdiggory@atmire.com>
2014-10-29 05:14:48 -0400

    Seems only varchar works in this case.

--- dspace/etc/postgres/dataone.sql
+++ dspace/etc/postgres/dataone.sql
@@ -24,7 +24,7 @@ ALTER TABLE versionitem DROP CONSTRAINT versionitem_item_id_fkey;
 —-----------------
 -- Bitstream Improvements (Created, last modified Triggers, char based uuid column)
 —-----------------
-alter table bitstream add column uuid CHAR(36);
+alter table bitstream add column uuid VARCHAR(36);
 alter table bitstream add column create_date timestamp default current_timestamp;
 alter table bitstream add column last_modified_date timestamp default current_timestamp;
 
